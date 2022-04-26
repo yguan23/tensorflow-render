061f9003b98e6f71d5d6cd86cacd8237d0bbee1b
yguan23 <103955839+yguan23@users.noreply.github.com>
2022-04-26 14:35:24 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -6,7 +6,7 @@ from fastai.vision import *
 import tensorflow as tf
 from PIL import Image
 from tensorflow import keras
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input
 import numpy as np
 #from tensorflow.keras.utils.data_utils import get_file
 from io import BytesIO
